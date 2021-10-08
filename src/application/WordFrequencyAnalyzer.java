3254d0adec678831e24b66a4111303191d205342
Derek DiLeo <derekfdileo@gmail.com>
2021-10-08 13:48:18 -0400

    Add JavaDoc comments to WordFrequencyAnalyzer Class.

--- src/application/WordFrequencyAnalyzer.java
+++ src/application/WordFrequencyAnalyzer.java
@@ -4,14 +4,24 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A Class which instantiates a Word object with each entry of HashMap (K/V = word/frequency) 
+ * and adds to an ArrayList<Word> to enable sorting and printing (Word implements Comparable<Word>).   
+ * @author Derek DiLeo */
 public class WordFrequencyAnalyzer {
 	
-	// Method to parse HashMap key/value pairs and push to an ArrayList
+	/**
+	 * Method instantiates a Word object with each entry of HashMap (K/V = word/frequency) 
+     * then adds all to (and returns) an ArrayList<Word>.  
+	 * @param hm is a HashMap<String, Integer> with Key/Value = word/frequency 
+	 * @return ArrayList<Word> which enables the sorting and printing of each 
+	 * entry(Word implements Comparable<Word>). */
 	static ArrayList<Word> processHashMap(HashMap<String, Integer> hm) {
 
-		// ArrayList to hold all new Word objects created prior to sorting by frequency (value)
+		// ArrayList to hold Word objects for later comparison
 		ArrayList<Word> wordsUnsorted = new ArrayList<Word>();
 
+		// Loop through each entry in HashMap
 		for (@SuppressWarnings("rawtypes")
 		Map.Entry entry : hm.entrySet()) {
 
