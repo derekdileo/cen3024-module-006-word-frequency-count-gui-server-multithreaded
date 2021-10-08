bd991b01882f7c61907b699184a8ec5659be9bc0
Derek DiLeo <derekfdileo@gmail.com>
2021-10-08 16:07:30 -0400

    Add JavaDoc comments to Word Class and remove unnecessary setters.

--- src/application/Word.java
+++ src/application/Word.java
@@ -1,5 +1,8 @@
 package application;
 
+/** Word Class objects are created from HashMap<String, Integer> (key=word, value=frequency) and 
+ * implements Comparable<Word> so ArrayList<Word> can be sorted and overrides toString() to print results to console. 
+ * @author derekdileo */
 public class Word implements Comparable<Word> {
 
 	// Local variables
@@ -13,31 +16,25 @@ public class Word implements Comparable<Word> {
 		this.frequency = value;
 	}
 	
-	// Getters and Setters
+	// Getters
 	public String getKey() {
 		return word;
-	}
-
-	public void setKey(String key) {
-		this.word = key;
 	}
 
 	public int getValue() {
 		return frequency;
 	}
 
-	public void setValue(int value) {
-		this.frequency = value;
-	}
-
-	// Implemented Method that allows Word objects to be compared
-	// by frequency (value)
+	/**Implemented Method that allows Word objects to be compared
+	 * in order to allow ArrayList<Word> to be sorted by value (frequency). */
 	@Override
 	public int compareTo(Word word) {
 		return this.frequency - word.getValue();
 	}
 
-	// toString Method takes index value for final output of values to console
+	/** Method which takes index value (in sorted list) for final output of values to console and GUI
+	 * @param index is index location in ArrayList<Word> sorted by frequency (value)  
+	 * @return returns a String to be printed to console and GUI */
 	public String toString(int index) {
 		return "\n" + (index + 1) + ") Word: " + word + "\t\tFrequency: " + frequency;
 	}
