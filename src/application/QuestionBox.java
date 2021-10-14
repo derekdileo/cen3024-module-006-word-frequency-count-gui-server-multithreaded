7159712e1d8347f422cff687a6f5e48ce6425474
Derek DiLeo <derekfdileo@gmail.com>
2021-10-14 12:32:16 -0400

    Create easter egg. Set testButton opacity to 0.0 and add functionality to change mouse cursor on hover. Also setFocusTraversable to false so tab button will not focus on it.

--- src/application/QuestionBox.java
+++ src/application/QuestionBox.java
@@ -2,6 +2,7 @@ package application;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,6 +11,8 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -50,22 +53,20 @@ public class QuestionBox {
         window.setMinWidth(250);
         window.setMinHeight(100);
 
-        // Create a label to display the String message parameter which is passed from caller
+        // Create a label to display prompts[1] passed from caller
         Label header = new Label();
         header.setText(prompts[1]);
         header.setFocusTraversable(true);
-        header.setStyle("-fx-font-size: 13pt;");
-        //header.setStyle("-fx-font-weight: bold;");
+        header.setFont(Font.font("Avenir", FontWeight.BOLD, 16));
         
         // Create text fields for user input
         Label siteLabel = new Label();
-        siteLabel.setId("site-label");
         siteLabel.setText("Website to Parse");
         TextField siteField = new TextField();
         siteField.setPromptText(prompts[2]);
         
         Label startLabel = new Label();
-        startLabel.setText("Where should it start?");
+        startLabel.setText("Where to start.");
         TextField startField = new TextField();
         startField.setPromptText(prompts[3]);
         
@@ -74,13 +75,28 @@ public class QuestionBox {
         TextField endField = new TextField();
         endField.setPromptText(prompts[4]);
 
-        // Create two buttons
+        // Create two buttons and set their behavior
         Button submitButton = new Button("Submit");
-        Button testButton = new Button("   ");
+        Button testButton = new Button("    ");
         
-        // Handle when "Submit" button clicked
+        // Make testButton an easter egg (hover will change cursor)
+        // see bottom for testButton.setOnMouseEntered/Exited Methods
+        testButton.setOpacity(0.00); 
+        testButton.setFocusTraversable(false);
+        
+        // When "Submit" button clicked, collect user responses
         submitButton.setOnAction(e -> {
-            responses[0] = siteField.getText();
+            // test for valid URL
+//        	String site = siteField.getText();
+//        	try {
+//				@SuppressWarnings("unused")
+//				URL url = new URL(site);
+//			} catch (MalformedURLException e1) {
+//				AlertBox.display("Error", "Invalid URL");
+//				e1.printStackTrace();
+//			}
+
+        	responses[0] = siteField.getText();
             responses[1] = startField.getText();
             responses[2] = endField.getText();
             window.close();
@@ -93,7 +109,8 @@ public class QuestionBox {
             }
         });
         
-        // Handle when "test" button clicked
+        // When "Test" button clicked, use default entries
+        // Not focus-traversable, no ENTER key functionality
         testButton.setOnAction(e -> {
             responses[0] = defaultEntries[0];
             responses[1] = defaultEntries[1];
@@ -101,25 +118,15 @@ public class QuestionBox {
             window.close();
         });
         
-        // Allow Enter key to trigger "submit" button
-        submitButton.setOnKeyPressed(e -> {
-        	if (e.getCode().equals(KeyCode.ENTER)) {
-        		submitButton.fire();
-            }
-        });
-        
-        
         // Create an AnchorPane container for buttons at bottom
         AnchorPane buttons = new AnchorPane();
-        // Anchor them to either side of the Pane
-        AnchorPane.setLeftAnchor(submitButton, 20.0);
-        AnchorPane.setRightAnchor(testButton, 20.0);
-//        buttons.setPadding(new Insets(10,50,10,50));
-//        buttons.setStyle("-fx-hgap: 100;");
         
-        // Add all elements to layout, add VBox layout and stackPane2 to stackPane1
+        // Anchor them to either side of the Pane
+        AnchorPane.setLeftAnchor(submitButton, 0.0);
+        AnchorPane.setRightAnchor(testButton, 0.0);
+        
+        // Add all elements to layout
         buttons.getChildren().addAll(submitButton, testButton);
-        //buttons.setParent(Pos.CENTER);
         layout.getChildren().addAll(header, siteLabel, siteField, startLabel, startField, endLabel, endField, buttons);
         layout.setAlignment(Pos.CENTER_LEFT);
         qBoxStackPane1.getChildren().addAll(qBoxStackPane2, layout);
@@ -127,6 +134,16 @@ public class QuestionBox {
         // Create and set Scene from stackPane1
         Scene scene = new Scene(qBoxStackPane1);
         window.setScene(scene);
+        
+        // If mouse hovers over button, change cursor
+        testButton.setOnMouseEntered(e -> {
+    		scene.setCursor(Cursor.HAND);
+        });
+        
+        // When mouse leaves, change it back
+        testButton.setOnMouseExited(e -> {
+        	scene.setCursor(Cursor.DEFAULT);
+        });
 
         // Show window and wait for user interaction
         window.showAndWait();
