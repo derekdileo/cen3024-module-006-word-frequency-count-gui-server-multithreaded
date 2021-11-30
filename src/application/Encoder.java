a67dbf5423dc75d2317528b67b34f45ea71c24de
Derek DiLeo <derekfdileo@gmail.com>
2021-11-30 12:42:27 -0500

    Add commets to Encoder class (which is not used, but may come in handy in the future).

--- src/application/Encoder.java
+++ src/application/Encoder.java
@@ -4,18 +4,18 @@ import java.io.UnsupportedEncodingException;
 
 public class Encoder {
 
-	/**
-	 * @param strings
-	 * @return
+	/** An attempt to encode String array for transfer over client / server connection.
+	 * @param strings user-entered URL, start, finish points
+	 * @return byte array for transfer from client to server via Socket
 	 * @throws UnsupportedEncodingException
 	 */
 	public byte[] encode(String[] strings) throws UnsupportedEncodingException {
 		return String.join(",", strings).getBytes("UTF_8");
 	}
 	
-	/**
-	 * @param encodedArray
-	 * @return
+	/** An attempt to decode byte array after transfer over client / server connection.
+	 * @param encoded byte array which was transferred from client to server
+	 * @return String array which contains user-entered URL, start, finish points to be used in WebScrape class
 	 * @throws UnsupportedEncodingException
 	 */
 	public String[] decode(byte[] encodedArray) throws UnsupportedEncodingException {
